51f94f0fe4077b1a93524199c70b91d3b73d6ece
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-12-11 04:19:37 +0000

    task 9 and 10: done and dusted

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for City class
+"""
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class
+    """
+    state_id = ''
+    name = ''
