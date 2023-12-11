9d785b899cd13144d557de762a6b7ea2fed23bf5
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-12-11 02:37:26 +0000

    task 7: bugs fixed

--- README.md
+++ README.md
@@ -1,18 +1,57 @@
 0x00. AirBnB clone - The console
 
-Welcome to the AirBnB clone project!
-Before starting, please read the AirBnB concept page.
 
-First step: Write a command interpreter to manage your AirBnB objects.
-This is the first step towards building your first full web application:
-the AirBnB clone. This first step is very important because you will use what
-you build during this project with all other following projects:
-HTML/CSS templating, database storage, API, front-end integration…
+AirBnB Clone Project
+This is an Airbnb clone project, designed to emulate some of the functionalities
+of the popular Airbnb platform. The project includes a simple command-line
+interface (CLI) that allows users to interact with the system using various commands.
 
-Each task is linked and will help you to:
+*Table of Contents*
+Installation
+Usage
+Commands
+Contributing
+License
 
-put in place a parent class (called BaseModel) to take care of the initialization, serialization and deserialization of your future instances
-create a simple flow of serialization/deserialization: Instance <-> Dictionary <-> JSON string <-> file
-create all classes used for AirBnB (User, State, City, Place…) that inherit from BaseModel
-create the first abstracted storage engine of the project: File storage.
-create all unittests to validate all our classes and storage engine
+
+*Installation*
+To install and run the Airbnb clone project, follow these steps:
+
+Clone the repository to your local machine:
+
+git clone https://github.com/your-username/AirBnB_clone.git
+Navigate to the project directory:
+
+cd AirBnB_clone
+Run the command-line interface:
+
+./console.py
+
+
+*Usage*
+The Airbnb clone CLI provides a set of commands to create, view, and manage
+instances of different classes. Use the help command to get information about available commands.
+
+(hbnb) help
+
+
+*Commands*
+The following commands are available in the Airbnb clone CLI:
+
+create: Creates a new instance of a specified class.
+show: Prints the string representation of an instance based on the class name and ID.
+destroy: Deletes an instance based on the class name and ID.
+all: Prints all string representations of instances based on the class name.
+update: Updates an instance based on the class name and ID by adding or updating attributes.
+For detailed information on each command, use the help command followed by the command name:
+
+(hbnb) help create
+
+
+*Contributing*
+Contributions to the Airbnb clone project are welcome. If you find a bug or
+have a suggestion, please open an issue or submit a pull request.
+
+
+*Authors*
+Wisdom Ikegwuru
--- console.py
+++ console.py
@@ -6,6 +6,9 @@ command interpreter
 
 
 import cmd
+import models
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -13,6 +16,7 @@ class HBNBCommand(cmd.Cmd):
     command interpreter
     """
     prompt = "(hbnb) "
+    rec_classes = ["BaseModel", "User", "City", "State", "Review", "Amenity"]
 
     def do_quit(self, arg):
         """
@@ -31,6 +35,110 @@ class HBNBCommand(cmd.Cmd):
         Does nothing for an empty line
         """
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.rec_classes:
+            print("** class doesn't exist **")
+        else:
+            instance = eval(f"{args[0]}()")
+            storage.save()
+            print(instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the
+        class name and id
+        """
+        arg_split = arg.split()
+        if not arg_split:
+            print('** class name missing **')
+        else:
+            class_name = arg_split[0]
+            if class_name not in self.rec_classes:
+                print("** class doesn't exist **")
+            elif len(arg_split) < 2:
+                print("** instance id missing **")
+            else:
+                obj_id = arg_split[1]
+                key = f"{class_name}.{obj_id}"
+                if key in storage.all():
+                    print(storage.all()[key])
+                else:
+                    print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file)
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        else:
+            class_name = args[0]
+            if class_name not in self.rec_classes:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                obj_id = args[1]
+                key = f"{class_name}.{obj.id}"
+                if key in storage.all():
+                    del storage.all()[key]
+                    storage.save()
+                else:
+                    print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based
+        or not on the class name
+        """
+        args = arg.split()
+        if not args:
+            for obj in storage.all().value():
+                print(str(obj))
+        elif args[0] not in self.rec_classes:
+            print("** class doesn't exist **")
+        else:
+            for obj in storage.all().values():
+                if obj.__class__.__name__ == args[0]:
+                    print(str(obj))
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.rec_classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = args[1]
+            key = f"{args[0]}.{obj_id}"
+            if key not in storage.all():
+                print("** no instance found **")
+            elif len(args) < 3:
+                print("** attribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            else:
+                instance = storage.all()[key]
+                attr_name = args[2]
+                attr_value = args[3]
+                setattr(instance, attr_name, attr_value)
+            storage.save()
 
 
 if __name__ == "__main__":
